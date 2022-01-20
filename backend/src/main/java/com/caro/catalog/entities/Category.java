de3fe45afbed00c4b341d24ee6c715d463b0d9d2
Carolina <argente2019@gmail.com>
2022-01-20 11:09:44 -0300

    H2 database

--- backend/src/main/java/com/caro/catalog/entities/Category.java
+++ backend/src/main/java/com/caro/catalog/entities/Category.java
@@ -1,8 +1,15 @@
 package com.caro.catalog.entities;
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name="tb_category")
 public class Category implements Serializable {
+    private static final long serialVersionUID=1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
@@ -13,6 +20,28 @@ public class Category implements Serializable {
         this.id = id;
         this.name = name;
     }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
 
     @Override
     public boolean equals(Object o) {
