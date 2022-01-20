65c5017186674eefdc2cb5c8f77e43c1a7433a88
Carolina <argente2019@gmail.com>
2022-01-20 17:31:42 -0300

    Navbar styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,9 @@
-import "./assets/styles/custom.scss";
-import "./App.css";
+import './assets/styles/custom.scss';
+import './App.css';
+import Navbar from './components/Navbar';
 
-function App() {
-	return <h1>Product Catalog</h1>;
-}
+const App = () => {
+  return <Navbar />;
+};
 
 export default App;
