70e74e3fec1b27d35a62893feb0fcc6cbe2a748d
Carolina <argente2019@gmail.com>
2022-01-20 11:40:39 -0300

    DTO

--- backend/src/main/java/com/caro/catalog/services/CategoryService.java
+++ backend/src/main/java/com/caro/catalog/services/CategoryService.java
@@ -1,11 +1,14 @@
 package com.caro.catalog.services;
 
+import com.caro.catalog.dto.CategoryDTO;
 import com.caro.catalog.entities.Category;
 import com.caro.catalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
@@ -13,7 +16,15 @@ public class CategoryService {
     @Autowired
     private CategoryRepository repository;
 
-    public List<Category> findAll(){
-        return repository.findAll();
+    public List<CategoryDTO> findAll(){
+        List<Category> list=repository.findAll();
+        return list.stream().map(x->new CategoryDTO(x)).collect(Collectors.toList());
+
+       /*//===== the stream line above does the same as below:
+        List<CategoryDTO> listDto = new ArrayList<>();
+        for(Category cat:list){
+            listDto.add(new CategoryDTO(cat));
+        }
+        return listDto;*/
     }
 }
