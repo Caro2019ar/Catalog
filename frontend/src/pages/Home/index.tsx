f45c135f5c77559d29d3a82aedd855f860187d22
Carolina <argente2019@gmail.com>
2022-01-20 18:37:16 -0300

    Home and button styling

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import './styles.css';
 import { ReactComponent as MainImage } from 'assets/images/main-image.svg';
 import Navbar from 'components/Navbar';
+import ButtonIcon from 'components/ButtonIcon';
 
 const Home = () => {
   return (
@@ -10,7 +11,8 @@ const Home = () => {
         <div className="home-card">
           <div className="home-content-container">
             <h1>Find the best catalog of products</h1>
-            <span>We will help you find the best products in the market</span>
+            <p>We will help you find the best products in the market</p>
+            <ButtonIcon />
           </div>
 
           <div className="home-image-container">
