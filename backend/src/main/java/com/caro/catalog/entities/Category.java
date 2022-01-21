dd9140a1f262551b5b2c1dc035991f7caeb8af25
Carolina <argente2019@gmail.com>
2022-01-21 12:16:02 -0300

    UTC PrePersist timezone

--- backend/src/main/java/com/caro/catalog/entities/Category.java
+++ backend/src/main/java/com/caro/catalog/entities/Category.java
@@ -1,6 +1,7 @@
 package com.caro.catalog.entities;
 import javax.persistence.*;
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.Objects;
 
 @Entity
@@ -12,6 +13,30 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant createdAt;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant updatedAt;
+    
+    @PrePersist
+    public void prePersist(){
+        createdAt=Instant.now();
+    }
+    @PreUpdate
+    public void preUpdate(){
+        updatedAt=Instant.now();
+    }
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+
 
     public Category() {
     }
